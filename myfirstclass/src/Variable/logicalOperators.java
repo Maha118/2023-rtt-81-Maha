4e773ce9acd871b0279bc9eb49f8447ee20cf2c1
Mahelet G <maheletpaul@gmail.com>
2023-09-21 17:46:38 -0400

    Operatores

---
+++ myfirstclass/src/Variable/logicalOperators.java
@@ -0,0 +1,17 @@
+package Variable;
+
+public class logicalOperators {
+	public static void main(String[] args)
+	   {
+	       boolean x = true;
+	       boolean y = false;
+	       System.out.println("x & y : " + (x & y));
+	       System.out.println("x && y : " + (x && y));
+	       System.out.println("x | y : " + (x | y));
+	       System.out.println("x || y: " + (x || y));
+	       System.out.println("x ^ y : " + (x ^ y));
+	       System.out.println("!x : " + (!x));
+	   }
+
+
+}
