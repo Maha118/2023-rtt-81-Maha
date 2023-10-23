a960622d721863a6e056669cf753b4d6d61d895c
Mimi118 <maheletpaul@gmail.com>
2023-10-22 21:26:37 -0700

    sql commit

---
+++ database/src/main/java/perscholasdatabase/firstconnection.java
@@ -0,0 +1,52 @@
+package perscholasdatabase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class firstconnection {
+	public static void main(String[] args) throws SQLException {
+		String dburl = "jdbc:mysql://localhost:3306/classicmodels";
+		String user = "root";
+		String password = "mahelet1975$";
+
+		Connection connection = null;
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver"); // optional
+			connection = DriverManager.getConnection(dburl, user, password);
+
+			try (Scanner scanner = new Scanner(System.in)) {
+				System.out.println("Enter an employee first name :");
+				String firstname = scanner.nextLine();
+
+				String sql = "Select * FROM employees where firstname = '" + firstname = ? AND lastname = ? + "'" ;
+				System.out.println(sql);
+
+				Statement stmt = connection.createStatement();
+
+				ResultSet result = stmt.executeQuery(sql);
+
+				while (result.next()) {
+					Integer id = result.getInt("id");
+					String name = result.getString("firstname");
+					String name1 = result.getString("lastname");
+					String email = result.getString("email");
+					System.out.println(id + " | " + name + " | " + "lname" + "|" + email);
+				}
+
+				result.close();
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (connection != null) {
+				connection.close();
+			}
+		}
+	}
+}
